ce30079c3d502b75428103bedc03bbb430bed4f4
copiner <810023494@qq.com>
2019-03-19 16:57:46 +0800

    soon

--- service/index.js
+++ service/index.js
@@ -1,10 +1,22 @@
 module.exports = {
     register: async(name, pwd) => {
-      let data 
+      let data
       if (name == 'wdaonngg' && pwd == '123456') {
-        data = `Hello, ${name}!`
+        data = {
+          status:0,
+          data:{
+            title:"个人中心",
+            content:"欢迎来到个人中心"
+          }
+        }
       } else {
-          data = 'error';
+        data = {
+          status:-1,
+          data:{
+            title:"登陆失败",
+            content:"请输入正确的账号信息"
+          }
+        }
       }
       return data
     }
