bb5e85022ffdd6ae5ce9ffcd9619ac3332a9ba94
copiner <810023494@qq.com>
2019-03-20 13:36:49 +0800

    log

--- middleware/index.js
+++ middleware/index.js
@@ -2,9 +2,20 @@ const path = require('path');
 const bodyParser = require('koa-bodyparser');
 const nunjucks = require('koa-nunjucks-2');
 const staticFiles = require('koa-static');
+const ip = require('ip');
 
 const miSend = require('./mi-send');
+const miLog = require('./mi-log')
+
 module.export = (app)=>{
+    app.use(miLog({
+      env: app.env,  // koa 提供的环境变量
+      projectName: 'koa2-tutor',
+      appLogLevel: 'debug',
+      dir: 'logs',
+      serverIp: ip.address()
+    }))
+
     app.use(staticFiles(path.resolve(__dirname,"./public")));
 
     app.use(nunjucks({
@@ -14,7 +25,7 @@ module.export = (app)=>{
     	     trimBlocks:true
         }
     }));
-    
+
     app.use(bodyParser());
 
     app.use(miSend())
