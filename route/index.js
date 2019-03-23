6688e2ed518ad22b6eaacd709c253ea9d675580e
copiner <810023494@qq.com>
2019-03-23 15:25:33 +0800

    to0323SQL

--- route/index.js
+++ route/index.js
@@ -3,7 +3,7 @@ module.exports = (app) => {
   console.log(app.controller.index.index)
   router.get( '/', app.controller.index.index )
   router.get('/home', app.controller.index.home)
-  router.get('/home/:id/:name', app.controller.index.about)
+  router.get('/about/:id/:name', app.controller.index.about)
   router.get('/user', app.controller.index.login)
   router.post('/user/register', app.controller.index.register)
   app.use(router.routes())
